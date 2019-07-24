93f340e4a555daeaeaf9bd6a96ee79f18d71d254
jrl508 <jrl47@yahoo.com>
2019-07-24 17:55:06 -0400

    mvp2 - added sessions

--- apiRouter.js
+++ apiRouter.js
@@ -28,7 +28,8 @@ router.post('/login', (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
-          res.status(200).json({ message: `Welcome ${user.username}!` });
+            req.session.user = user;
+            res.status(200).json({ message: `Welcome ${user.username}!` });
         } else {
           res.status(401).json({ message: 'Invalid Credentials' });
         }
